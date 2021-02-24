4a06006d975f879338feeeff56f4584e691f1606
Francesco Baldi <francesco.baldi1987@gmail.com>
2021-02-24 16:06:46 +0100

    persistence

--- jpa-service-plugin-generator/src/main/java/bld/plugin/jpa/service/generator/classes/ClassBuilding.java
+++ jpa-service-plugin-generator/src/main/java/bld/plugin/jpa/service/generator/classes/ClassBuilding.java
@@ -198,10 +198,10 @@ public class ClassBuilding {
 				
 			}else if (field.isAnnotationPresent(Column.class)) {
 				if(Calendar.class.isAssignableFrom(field.getType()) || Date.class.isAssignableFrom(field.getType()) || Timestamp.class.isAssignableFrom(field.getType())) {
-					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"BeforeEqual\", \" and "+fieldEntity+"."+field.getName()+">=:"+field.getName()+"BeforeEqual \");");
-					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"AfterEqual\", \" and "+fieldEntity+"."+field.getName()+"<=:"+field.getName()+"AfterEqual \");");
-					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"Before\", \" and "+fieldEntity+"."+field.getName()+">:"+field.getName()+"Before \");");
-					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"After\", \" and "+fieldEntity+"."+field.getName()+"<:"+field.getName()+"After \");");
+					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"BeforeEqual\", \" and "+fieldEntity+"."+field.getName()+"<=:"+field.getName()+"BeforeEqual \");");
+					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"AfterEqual\", \" and "+fieldEntity+"."+field.getName()+">=:"+field.getName()+"AfterEqual \");");
+					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"Before\", \" and "+fieldEntity+"."+field.getName()+"<:"+field.getName()+"Before \");");
+					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"After\", \" and "+fieldEntity+"."+field.getName()+">:"+field.getName()+"After \");");
 					mapConditions.add(SPACE+"map.put(\"" + field.getName()+"\", \" and "+fieldEntity+"."+field.getName()+"=:"+field.getName()+" \");");
 					
 				}else if(String.class.isAssignableFrom(field.getType())){
