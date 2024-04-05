138f541d00cbd4ba8c74d8816901f78c0654ba9a
Francesco Baldi <francesco.baldi1987@gmail.com>
2024-04-05 09:37:03 +0200

    library

--- proxy-api-controller/src/main/java/com/bld/proxy/api/find/intecerptor/ApiFindInterceptor.java
+++ proxy-api-controller/src/main/java/com/bld/proxy/api/find/intecerptor/ApiFindInterceptor.java
@@ -5,27 +5,29 @@ import java.lang.reflect.Method;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
 
-@Component
+import com.bld.proxy.api.find.data.ApiMethod;
+
 public class ApiFindInterceptor implements InvocationHandler {
-
 
 	private final static Logger logger = LoggerFactory.getLogger(ApiFindInterceptor.class);
 
-	@Autowired
-	private ApplicationContext applicationContext;
+	private final FindInterceptor findInterceptor;
 
-	
+
+	public ApiFindInterceptor(FindInterceptor findInterceptor) {
+		super();
+		this.findInterceptor = findInterceptor;
+	}
+
+
+
 	@Override
 	public Object invoke(Object obj, Method method, Object[] args) throws Throwable {
 		if ("hashCode".equalsIgnoreCase(method.getName()))
 			return 0;
 		logger.info("invoke");
-		FindInterceptor findInterceptor = this.applicationContext.getBean(FindInterceptor.class);
-		return findInterceptor.find(obj, method, args);
+		return findInterceptor.find(obj, new ApiMethod(method, args));
 	}
 
 }
